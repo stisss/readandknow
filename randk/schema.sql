3bea1e20651d3967a66543c8aa9a69501dcb8002
stisss <blazejstiskun@gmail.com>
2020-01-28 05:19:40 +0100

    Database modification - reactions, saved, followers

--- randk/schema.sql
+++ randk/schema.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS article;
 DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS saved;
+DROP TABLE IF EXISTS followers;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,6 +22,7 @@ CREATE TABLE article (
   description TEXT,
   body TEXT NOT NULL,
   magazine TEXT,
+  reactions INTEGER DEFAULT 0,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
@@ -29,4 +32,21 @@ CREATE TABLE comment (
   body TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE saved (
+  id_user INTEGER,
+  id_article INTEGER,
+  FOREIGN KEY (id_user) REFERENCES user (id),
+  FOREIGN KEY (id_article) REFERENCES article (id),
+  PRIMARY KEY (id_user, id_article)
+);
+
+
+CREATE TABLE followers (
+  id_follower INTEGER,
+  id_followed INTEGER,
+  FOREIGN KEY (id_follower) REFERENCES user (id),
+  FOREIGN KEY (id_followed) REFERENCES user (id),
+  PRIMARY KEY (id_follower, id_followed)
 );
